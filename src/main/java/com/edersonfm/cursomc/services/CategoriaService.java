1041e8748ace9ca1dd13b8421de8b481f7b3c60f
edersonmangueira <ederson.mangueira@hotmail.com>
2019-02-27 17:10:46 -0300

    Validação sintatica com o Bean Validation

--- src/main/java/com/edersonfm/cursomc/services/CategoriaService.java
+++ src/main/java/com/edersonfm/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edersonfm.cursomc.domain.Categoria;
+import com.edersonfm.cursomc.dto.CategoriaDTO;
 import com.edersonfm.cursomc.repositories.CategoriaRepository;
 import com.edersonfm.cursomc.services.exceptions.DataIntegrityException;
 import com.edersonfm.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),	orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO categoriaDto) {
+		return new Categoria(categoriaDto.getId(),categoriaDto.getNome());
 	}
 
 }
